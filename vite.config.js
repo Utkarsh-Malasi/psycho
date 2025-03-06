02edbec7dcd37740aebf70201cfd1a76bcf441d0
Utkarsh Malasi <UtkarshMalasi@gmail.com>
2025-03-06 14:42:42 +0530

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-})
+  base: '/', // Adjust this if your app is served from a subdirectory
+  build: {
+    outDir: 'dist', // Output directory for the build
+    assetsDir: 'assets', // Directory for static assets
+  }
+});
